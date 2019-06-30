436df845f8d1840c7f407e1e900fa932ab36e5a2
irfa89 <Admin@123>
2019-06-29 20:46:40 -0400

    Initial commit

---
+++ src/Map.java
@@ -0,0 +1,20 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+public class Map extends Mapper<LongWritable, Text,Text,IntWritable> {
+
+    @Override
+    public void map(LongWritable key,Text value,Context context)
+            throws IOException,InterruptedException{
+
+        String[] row = value.toString().split("\t");
+        context.write(new Text(row[2]),new IntWritable(1));
+
+    }
+
+}
